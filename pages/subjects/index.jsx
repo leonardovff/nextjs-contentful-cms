8c65e44ff97d0c7ba14f82c9985f5108483c6368
leonardovff <leonardovff@gmail.com>
2023-03-08 17:29:35 -0300

    Test new carousel component

--- pages/subjects/index.jsx
+++ pages/subjects/index.jsx
@@ -1,54 +1,79 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
 // import { useState, useEffect } from 'react';
-import Link from 'next/link';
 // import Image from 'next/image';
-import Head from 'next/head';
 // import Script from 'next/script';
+import Link from 'next/link';
+import Head from 'next/head';
+// import '../../styles/globals.css';
 import Layout from '../../components/layout';
 import { getSubjectLists } from '../../modules/subject';
 import { Subjects } from '../../components/subjects';
-// import '../../styles/globals.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+const ThemeContext = React.createContext('light');
+
+function Father({teste, children}){
+    console.log(teste);
+    return <>{children}</>;
+}
+function ChildA({children}){
+    return <>{children}</>;
+}
+function ChildB({children}){
+    return <><ChildC></ChildC></>;
+}
+
+function ChildC({children}){
+    const value = useContext(ThemeContext);
+    return <><Title>Oláaaaa, {value}</Title></>;
+}
 
 export default function SubjectsPage ({subjects}) {
-    // const [subjects, setSubjects] = useState([]);
-    // useEffect(() => {
-    //     fetch('https://my-json-server.typicode.com/typicode/demo/posts')
-    //         .then(res => res.json())
-    //         .then(data => setSubjects(data))
-    // }, []);
-    
+    const teste= 'olá';
     return <Layout>
         <Head>
             <title>List of Subjects</title>
         </Head>
-        {/* <Script 
-            strategy="lazyOnload"
-            onLoad={() =>
-                console.log(`script loaded correctly, window.FB has been populated`)
-            }
-            src="https://connect.facebook.net/en_US/sdk.js" 
-        /> */}
+        <Title>Subjects</Title>
         <Link href="/">Back to home</Link>
-        {/* <img src="/images/profile.jpeg" /> */}
-        {/* <Image
-            src="/images/profile.jpeg" // Route of the image file
-            height={444}
-            width={444}
-            alt="Your Name"
-        /> */}
-       <Subjects subjects={subjects}></Subjects>
+        <Subjects subjects={subjects}></Subjects>
+        <ThemeContext.Provider>
+            <Father teste={teste}>
+                <ChildA>
+                    <ChildB></ChildB>
+                </ChildA>
+            </Father>
+        </ThemeContext.Provider>
     </Layout>
 }
 
 export async function getServerSideProps() {
-    // Get external data from the file system, API, DB, etc.
-    // const subjects = await fetch('https://my-json-server.typicode.com/typicode/demo/posts')
-    //         .then(res => res.json());
     const { items } = await getSubjectLists();
-    // The value of the `props` key will be
-    //  passed to the `Home` component
     return {
       props: {
         subjects: items
       }
     }
-  }
+}
+
+// const [subjects, setSubjects] = useState([]);
+// useEffect(() => {
+//     fetch('https://my-json-server.typicode.com/typicode/demo/posts')
+//         .then(res => res.json())
+//         .then(data => setSubjects(data))
+// }, []);
+// Get external data from the file system, API, DB, etc.
+// const subjects = await fetch('https://my-json-server.typicode.com/typicode/demo/posts')
+//         .then(res => res.json());
+/* <Script 
+    strategy="lazyOnload"
+    onLoad={() =>
+        console.log(`script loaded correctly, window.FB has been populated`)
+    }
+    src="https://connect.facebook.net/en_US/sdk.js" 
+/> */
