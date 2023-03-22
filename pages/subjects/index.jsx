e47f6e014caf99fd0c6f251fc5f2f6525d1c5d55
leonardovff <leonardovff@gmail.com>
2023-03-22 17:26:47 -0300

    Fix user authentication

--- pages/subjects/index.jsx
+++ pages/subjects/index.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
-// import { useState, useEffect } from 'react';
-// import Image from 'next/image';
-// import Script from 'next/script';
+
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./../api/auth/[...nextauth]"
+
 import Link from 'next/link';
 import Head from 'next/head';
 // import '../../styles/globals.css';
@@ -10,72 +11,42 @@ import Layout from '../../components/layout';
 import { getSubjectLists } from '../../modules/subject';
 import { Subjects } from '../../components/subjects';
 import { Login } from "../../components/login";
+// import { getToken } from "next-auth/jwt";
+
 
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
 `;
-const ThemeContext = React.createContext('light');
+// const ThemeContext = React.createContext('light');
 
-function Father({teste, children}){
-    return <>{children}</>;
-}
-function ChildA({children}){
-    return <>{children}</>;
-}
-function ChildB({children}){
-    return <><ChildC></ChildC></>;
-}
-
-function ChildC({children}){
-    const value = useContext(ThemeContext);
-    return <><Title>Oláaaaa, {value}</Title></>;
-}
-
-export default function SubjectsPage ({subjects}) {
-    const teste= 'olá';
-    
+export default function SubjectsPage ({subjects, user}) {
     return <Layout>
         <Head>
             <title>List of Subjects</title>
         </Head>
+        <h3>Example using front-end verification (by calling the /api/auth/session endpoint)</h3>
         <Login/>
+
+        <h3>Example using server-side verification</h3>
+        {user ? <>
+            <p>{user.name}</p>
+            <a>Sing Out</a>
+        </> : <a>Sing In</a>}
         <Title>Subjects</Title>
         <Link href="/">Back to home</Link>
         <Subjects subjects={subjects}></Subjects>
-        <ThemeContext.Provider>
-            <Father teste={teste}>
-                <ChildA>
-                    <ChildB></ChildB>
-                </ChildA>
-            </Father>
-        </ThemeContext.Provider>
     </Layout>
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps({req, res}) {    
+    const session = await getServerSession(req, res, authOptions);
     const { items } = await getSubjectLists();
     return {
       props: {
-        subjects: items
+        subjects: items,
+        user: session? session.user : null
       }
     }
 }
-
-// const [subjects, setSubjects] = useState([]);
-// useEffect(() => {
-//     fetch('https://my-json-server.typicode.com/typicode/demo/posts')
-//         .then(res => res.json())
-//         .then(data => setSubjects(data))
-// }, []);
-// Get external data from the file system, API, DB, etc.
-// const subjects = await fetch('https://my-json-server.typicode.com/typicode/demo/posts')
-//         .then(res => res.json());
-/* <Script 
-    strategy="lazyOnload"
-    onLoad={() =>
-        console.log(`script loaded correctly, window.FB has been populated`)
-    }
-    src="https://connect.facebook.net/en_US/sdk.js" 
-/> */
